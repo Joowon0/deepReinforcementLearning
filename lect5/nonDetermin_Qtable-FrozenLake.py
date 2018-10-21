11c31fbc4b5c9afa0f3eec8448b79c5df27a956a
Joowon <joowon7508@gmail.com>
2018-10-22 00:02:45 +0900

    don't know why the new formula improves

---
+++ lect5/nonDetermin_Qtable-FrozenLake.py
@@ -0,0 +1,45 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+# game settings
+env = gym.make('FrozenLake-v0')
+
+# Initialize the whole table with zeros
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+
+# Set learning parameters
+learning_rate = .85
+dis = .99
+num_episodes = 2000
+
+# Create a list to contain rewards and steps for each episode
+rList = []
+for i in range(num_episodes):
+    # Reset environment and get first new observation
+    state = env.reset()
+    rewardAll = 0
+    done = False
+
+    # The Q-Table learning algorithm
+    while not done:
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i+1))
+
+        # Get new state and reward from environment
+        new_state, reward, done, _ = env.step(action)
+
+        # Update Q-Table with new knowledge using learning rate
+        Q[state, action] = (1-learning_rate) * Q[state,action] \
+                + learning_rate * (reward + dis * np.max(Q[new_state, :]))
+
+        rewardAll += reward
+        state = new_state
+
+    rList.append(rewardAll)
+
+print("Success rate: " + str(sum(rList)/num_episodes))
+print("Final Q-Table Values")
+print("LEFT DOWN RIGHT UP")
+print(Q)
+plt.bar(range(len(rList)), rList, color="blue")
+plt.show()
