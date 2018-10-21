8e6a42f7da869e15294d84b3a71b534d5d7a41a1
Joowon <joowon7508@gmail.com>
2018-10-21 23:15:43 +0900

    lect3: Exploit & Exploration E-greedy Discounted Reward

---
+++ lect4/randNoise_Qtable-FrozenLake.py
@@ -0,0 +1,55 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from gym.envs.registration import register
+import random as pr
+
+def rargmax(vector):
+    # argmax that chooses randomly among eligiable maximum indices
+    m = np.amax(vector)
+    indices = np.nonzero(vector == m)[0] # max값을 가지면서 0이 아닌 index들을 가져온다고???????
+    return pr.choice(indices)
+
+# game settings
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery' : False}
+)
+env = gym.make('FrozenLake-v3')
+
+# Initialize the whole table with zeros
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+# Set learning parameters
+dis = .99
+num_episodes = 2000
+
+# Create a list to contain rewards and steps for each episode
+rList = []
+for i in range(num_episodes):
+    # Reset environment and get first new observation
+    state = env.reset()
+    rewardAll = 0
+    done = False
+
+    # The Q-Table learning algorithm
+    while not done:
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (i+1))
+
+        # Get new state and reward from environment
+        new_state, reward, done, _ = env.step(action)
+
+        # Update Q-Table with new knowledge using learning rate
+        Q[state, action] = reward + dis * np.max(Q[new_state, :])
+
+        rewardAll += reward
+        state = new_state
+
+    rList.append(rewardAll)
+
+print("Success rate: " + str(sum(rList)/num_episodes))
+print("Final Q-Table Values")
+print("LEFT DOWN RIGHT UP")
+print(Q)
+plt.bar(range(len(rList)), rList, color="blue")
+plt.show()
